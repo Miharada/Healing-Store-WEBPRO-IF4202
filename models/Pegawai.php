0c6bd664ac733cada130ed00ac0cf0cec76f0dcb
TIWA RAMDHANI <41172181+Miharada@users.noreply.github.com>
2020-04-12 12:47:23 +0700

    Update Pegawai.php

--- models/Pegawai.php
+++ models/Pegawai.php
@@ -2,26 +2,26 @@
 class Pegawai extends CI_model{
 
     public function getAllPegawai(){
-        return $this->db->get('Pegawai')->result_array();
+        return $this->db->get('pgw')->result_array();
     }
 
     public function tambahPegawai($data){
-        return $this->db->insert('Pegawai',$data);
+        return $this->db->insert('pgw',$data);
     }
 
     public function hapusPegawai($id_pegawai){
         $this->db->where('id_pegawai',$id_pegawai);
-        return $this->db->delete('Pegawai');
+        return $this->db->delete('pgw');
     }
 
     public function editPegawai($data,$id_pegawai){
         $this->db->where('id_pegawai',$id_pegawai);
-        return $this->db->update('Pegawai',$data);
+        return $this->db->update('pgw',$data);
     }
     
     public function cariPegawai(){
         $keyword = $this->input->post('keyword',true);
-        return $this->db->get('Pegawai')->result_array();
+        return $this->db->get('pgw')->result_array();
     }
 }
 ?>
