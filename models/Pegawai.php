6bcf8eed346b8309d2dc1c4128b04c0a4ea1bcfe
TIWA RAMDHANI <41172181+Miharada@users.noreply.github.com>
2020-04-13 20:39:45 +0700

    Update Pegawai.php

--- models/Pegawai.php
+++ models/Pegawai.php
@@ -2,20 +2,36 @@
 class Pegawai extends CI_model{
 
     public function getAllPegawai(){
-        return $this->db->get('pgw')->result_array();
+        return $this->db->get('pgw')->result();
     }
 
-    public function tambahPegawai($data){
+    public function tambahPegawai(){
+        $data = array(
+            'id_pegawai' => $this->input->post('id_pegawai'),
+            'nama_pegawai' => $this->input->post('nama_pegawai'),
+            'alamat' => $this->input->post('alamat'),
+            'no_hp' => $this->input->post('no_hp')
+        );
         return $this->db->insert('pgw',$data);
     }
 
-    public function hapusPegawai($id_pegawai){
+    public function hapusPegawai(){
+        $id_pegawai = $this->input->post('id_pegawai');
         $this->db->where('id_pegawai',$id_pegawai);
         return $this->db->delete('pgw');
     }
 
-    public function editPegawai($data,$id_pegawai){
-        $this->db->where('id_pegawai',$id_pegawai);
+    public function editPegawai(){
+        $id = $this->input->post('id_pegawai');
+        $data=array(
+            'id_pegawai' => $this->input->post('id_pegawai'),
+            'nama_pegawai' => $this->input->post('nama_pegawai'),
+            'alamat' => $this->input->post('alamat'),
+            'no_hp' => $this->input->post('no_hp')
+        );
+     
+
+        $this->db->where('id_pegawai',$id);
         return $this->db->update('pgw',$data);
     }
     
