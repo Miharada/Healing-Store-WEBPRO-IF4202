959bf4e0653f2157dc8b8c1e9e9eb3648f4cdbf3
TIWA RAMDHANI <41172181+Miharada@users.noreply.github.com>
2020-04-13 20:40:19 +0700

    Update ctrl_pegawai.php

--- controllers/ctrl_pegawai.php
+++ controllers/ctrl_pegawai.php
@@ -9,28 +9,35 @@ class ctrl_pegawai extends CI_Controller
 		parent::__construct();
         $this->load->model('Pegawai');
 		$this->load->library('form_validation');
+		$this->load->helper('url');
 	}
 
 	public function index()
 	{
 		$data['judul'] = 'Daftar Pegawai';
 		$data['page'] = 'home_pegawai';
-		$data['data_pegawai'] = $this->Pegawai->getAllPegawai();
-		$this->load->view('home_pegawai',$data);
+		#$data['data_pegawai'] = $this->Pegawai->getAllPegawai();
+		$this->load->view('Pegawai/home_pegawai',$data);
 	}
 
-	public function tambah($data){
-		$this->Pegawai->tambahPegawai($data);
+	public function OutputPegawai(){
+		$content = $this->Pegawai->getAllPegawai();
+		echo json_encode($content);
 	}
 
-	public function hapus($id_pegawai){
-		$this->Pegawai->hapusPegawai($id_pegawai);
-		redirect('ctrl_pegawai');
+	public function tambah(){
+		$data = $this->Pegawai->tambahPegawai();
+		echo json_encode($data);
 	}
 
-	public function update($data, $id_pegawai){
-		$this->Pegawai->editPegawai($data,$id_pegawai);
-		redirect('ctrl_pegawai');
+	public function hapus(){
+		$data = $this->Pegawai->hapusPegawai();
+		echo json_encode($data);
+	}
+
+	public function update(){
+		$data = $this->Pegawai->editPegawai();
+		echo json_encode($data);
 	}
 
 }
